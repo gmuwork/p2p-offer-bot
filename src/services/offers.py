dad0062e0ff1b29e3f259c8fd1717bd901b4d0b3
Marko Prelevikj <pr3mar@users.noreply.github.com>
2023-04-25 20:53:32 +0200

    Add exception for no offers

--- src/services/offers.py
+++ src/services/offers.py
@@ -1,4 +1,5 @@
 import logging
+import simplejson
 
 from typing import List, Dict, Tuple
 
@@ -9,6 +10,12 @@ from src.clients.cmc import client as cmc_client
 
 logger = logging.getLogger(__name__)
 _LOG_PREFIX = "[NOONES-OFFERS]"
+
+
+class NoOffersException(Exception):
+    def __init__(self, params: Dict) -> None:
+        self.message = "No offers with given params found:\n"
+        self.message += f"{simplejson.dumps(params)}"
 
 
 def get_relevant_offers(
@@ -23,6 +30,16 @@ def get_relevant_offers(
     print(margin_low, margin_high, market_price)
 
     offers = get_latest_offers()
+    if not offers:
+        raise NoOffersException(
+            {
+                "crypto_currency": crypto_currency,
+                "fiat_currency": fiat_currency,
+                "margin_low": margin_low,
+                "margin_high": margin_high,
+                "market_price": market_price,
+            }
+        )
     relevant_offers = []
     for offer in offers:
         print(offer)
@@ -36,9 +53,10 @@ def get_relevant_offers(
 def get_latest_offers() -> List[Dict]:
     noonesAPI = noones_auth_client.NoonesApiClient()
     return noonesAPI.get_all_offers(
-        enums.OfferType.SELL,
-        enums.FiatCurrency.ZAR,
-        enums.PaymentMethod.BANK_TRANSFER,
+        offer_type=enums.OfferType.SELL,
+        crypto_currency=enums.CryptoCurrency.BTC,
+        conversion_currency=enums.FiatCurrency.ZAR,
+        payment_method=enums.PaymentMethod.BANK_TRANSFER,
     )
 
 
