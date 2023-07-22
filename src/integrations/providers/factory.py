431f901c6706da5654186a7d5007676a0c99e367
ghajduk3 <gh8590@student.uni-lj.si>
2023-07-22 12:55:14 +0200

    Added paxful auth integration client

--- src/integrations/providers/factory.py
+++ src/integrations/providers/factory.py
@@ -4,6 +4,7 @@ import typing
 from src import enums
 from src.integrations.providers import exceptions as provider_exceptions
 from src.integrations.providers.noones import client as noones_client
+from src.integrations.providers.paxful import client as paxful_client
 
 logger = logging.getLogger(__name__)
 
@@ -11,14 +12,15 @@ logger = logging.getLogger(__name__)
 class AuthenticationProviderFactory(object):
     _LOG_PREFIX = "[AUTHENTICATION-PROVIDER-FACTORY]"
     _PROVIDER_IMPLEMENTATION_MAP = {
-        enums.OfferProvider.NOONES: noones_client.NoonesAuthenticationProvider
+        enums.OfferProvider.NOONES: noones_client.NoonesAuthenticationProvider,
+        enums.OfferProvider.PAXFUL: paxful_client.PaxfulAuthenticationProvider,
     }
 
     def create(
         self, provider: enums.OfferProvider
     ) -> typing.Union[noones_client.NoonesAuthenticationProvider]:
         if provider not in self._PROVIDER_IMPLEMENTATION_MAP:
-            msg = "Provider {} is not supported".format(provider.name)
+            msg = "Authentication provider {} is not supported".format(provider.name)
             logger.error("{} {}.".format(self._LOG_PREFIX, msg))
             raise provider_exceptions.AuthenticationProviderNotSupportedError(msg)
 
--- src/integrations/providers/paxful/client.py
+++ src/integrations/providers/paxful/client.py
@@ -4,8 +4,8 @@ import typing
 
 from common import utils as common_utils
 from src import enums
-from src.integrations.gateways.noones import client as noones_client
-from src.integrations.gateways.noones import exceptions as noones_client_exceptions
+from src.integrations.gateways.paxful import client as paxful_client
+from src.integrations.gateways.paxful import exceptions as paxful_client_exceptions
 from src.integrations.providers import base as base_provider
 from src.integrations.providers import messages as provider_mesages
 from src.integrations.providers import exceptions as provider_exceptions
@@ -14,24 +14,24 @@ from src.integrations.providers.noones import schemas
 logger = logging.getLogger(__name__)
 
 
-class NoonesAuthenticationProvider(base_provider.BaseAuthenticationProvider):
-    _LOG_PREFIX = "[NOONES-AUTHENTICATION-PROVIDER]"
+class PaxfulAuthenticationProvider(base_provider.BaseAuthenticationProvider):
+    _LOG_PREFIX = "[PAXFUL-AUTHENTICATION-PROVIDER]"
 
     def __init__(self) -> None:
-        super(NoonesAuthenticationProvider, self).__init__()
+        super(PaxfulAuthenticationProvider, self).__init__()
 
     @property
     def provider(self) -> enums.OfferProvider:
-        return enums.OfferProvider.NOONES
+        return enums.OfferProvider.PAXFUL
 
     @property
     def provider_api_client_class(self) -> typing.Callable:
-        return noones_client.NoonesAuthAPIClient
+        return paxful_client.PaxfulAuthAPIClient
 
     def create_authentication_token(self) -> provider_mesages.AuthenticationToken:
         try:
             response = self.get_rest_api_client().create_authentication_token()
-        except noones_client_exceptions.NoonesAuthAPIException as e:
+        except paxful_client_exceptions.PaxfulAuthAPIException as e:
             msg = "Unable to create authentication token through provider API client. Error: {}".format(
                 common_utils.get_exception_message(exception=e)
             )
@@ -49,5 +49,5 @@ class NoonesAuthenticationProvider(base_provider.BaseAuthenticationProvider):
         return provider_mesages.AuthenticationToken(
             token=validated_data["access_token"],
             expires_at=datetime.datetime.now()
-            + datetime.timedelta(seconds=validated_data["expires_in"]),
+                       + datetime.timedelta(seconds=validated_data["expires_in"]),
         )
